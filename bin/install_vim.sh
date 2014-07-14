b842e760439cb101ee255938af546ff765e09805
root <root@AY13121810004468892bZ.(none)>
2014-07-14 16:06:46 +0800

    install vim

--- bin/install_vim.sh
+++ bin/install_vim.sh
@@ -1,11 +1,22 @@
-if[ ! -d ~/.vim/bundle/vundle ]
+if [ -e ~/.vim/bundle/vundle ]
 then
+  if [ -d ~/.vim/bundle/vundle ]
+  then
+    echo 'vundle.git '
+  fi
+else
   git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-fi
-if[ ! -d ~/git.base/my_linux ]
-then
-  git clone https://github.com/bukuta/my_linux.git ~/git.base/my_linux
-fi
-cp ~/git.base/my_linux/vim/.vimrc ~/.vimrc
-vim +VundleInstall
+  if [ ! -d ~/git.base/my_linux ]
+  then
+    git clone https://github.com/bukuta/my_linux.git ~/git.base/my_linux
+  fi
+  mv ~/.vimrc ~/.bak/.vimrc
+  cp ~/git.base/my_linux/vim/.vimrc ~/.vimrc
+  vim +VundleInstall
+  #检测别名是否存在
+  sed 's/\(g:NERDTreeDirArrows.*\)\(s:running_windows\)/\1!\2/' ~/.vim/bundle/The-NERD-tree/plugin/NERD_tree.vim >tmp
+  mv -f tmp ~/.vim/bundle/The-NERD-tree/plugin/NERD_tree.vim
 
+  echo "alias vj='vim +NERDTree'" >>~/.bashrc
+  source .bashrc
+fi
