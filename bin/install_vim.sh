1623d8e52992d8f559f731b2aa78010b55a334fe
bukuta <c7h12bo@gmail.com>
2014-07-22 14:44:01 +0800

    path modify

--- bin/install_vim.sh
+++ bin/install_vim.sh
@@ -6,12 +6,8 @@ then
   fi
 else
   git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-  if [ ! -d ~/git.base/my_linux ]
-  then
-    git clone https://github.com/bukuta/my_linux.git ~/git.base/my_linux
-  fi
   mv ~/.vimrc ~/.bak/.vimrc
-  cp ~/git.base/my_linux/vim/.vimrc ~/.vimrc
+  cp ../vim/.vimrc ~/.vimrc
   vim +VundleInstall
   #检测别名是否存在
   sed 's/\(g:NERDTreeDirArrows.*\)\(s:running_windows\)/\1!\2/' ~/.vim/bundle/The-NERD-tree/plugin/NERD_tree.vim >tmp
