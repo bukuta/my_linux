3cd1d9f36c1a8148a3fc16f6322a23ee7863254e
BoYuan <boyuan@BoYuandeMacBook-Pro.local>
2015-01-07 21:36:10 +0800

    remove xxx

--- bin/scripts/rsync_local2remote.sh
+++ bin/scripts/rsync_local2remote.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-rsync -rltouIzvc --exclude=".svn" --exclude="*.swp" ~/devspace/web/ yuanbo7@10.1.169.16:~/wdds/trunk/php/ffan/web/
-rsync -rltouIzvc --exclude=".svn" --exclude="*.swp" ~/devspace/javascript/ yuanbo7@10.1.169.16:~/wdds/trunk/javascript/
+rsync -rltouIzvc --exclude=".svn" --exclude="*.swp" ~/devspace/web/ user@server:~/php
+rsync -rltouIzvc --exclude=".svn" --exclude="*.swp" ~/devspace/javascript/ user@server:~/js
 exit 0;
 function get_project_root(){
   #从当前目录开始向上,查找当前目录下的.svn,直到没有.
