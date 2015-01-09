8f509c01ff4e30173295647583a9078d0e6a662c
bukuta <c7h12bo@gmail.com>
2015-01-09 21:45:48 +0800

    up

---
+++ bin/scripts/prepare_linux.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+yum -y install git gcc vim man mlocate make cmake cpack
+yum -y install zlib zlib-devel openssl opsnssl-devel pcre pcre-devel
+
+git clone https://github.com/nginx/nginx.git
+
+cd nginx
+configure
+make
+make install
