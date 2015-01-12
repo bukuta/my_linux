5998f54fd788e5249f8218cc8a7a3d4f49889a58
bukuta <c7h12bo@gmail.com>
2015-01-13 07:20:02 +0800

    node uglifyjs get define.id

---
+++ node/test.define.js
@@ -0,0 +1,11 @@
+/***
+ * aa
+ *
+ */
+define(["a.js","/aaa/bb.js"],function(a,bb){
+  var aa=123;
+  var bb=123;
+  var ananananananna=33;
+  console.log(ananananananna);
+  console.log(a,bb);
+});
